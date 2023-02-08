2d6798d921204f19f650377cb5a2db7f828c01ce
Mohd Ali Ansari <ansar326629@gmail.com>
2023-02-08 20:31:53 +0530

    fix/application_down
    - Added APP_NAME
    
    - Changed slotId to be the pK
    
    - Fixed createBooking and deleteBooking function
    
    - Fixed return value and error handling
    
    - Bumped version

--- controllers/parking.js
+++ controllers/parking.js
@@ -3,19 +3,19 @@ const parking = require('../models/parking')
 
 const getAllParkingSlots = (req, res) => {
     console.log("getting all parking slots")
-    res.status(200).send(parking);
+    return res.status(200).send(parking);
 }
 
 const getParkingSlotById = (req, res) => {
     const id = req.params.id;
-    const parking_slot = parking.filter((slot) => slot.slotId == id)
-    if (parking_slot.length===1){
+    const parked = parking.filter((slot) => slot.slotId == id)[0];
+    if (parked){
         console.log("getting parking slot")
-        res.status(200).send(parking_slot);
+        return res.status(200).send(parked);
     }
     else{
         console.error(`parking slot with id ${id} not exists`);
-        res.status(500).send({"error":`parking slot with id ${id} not exists`});
+        return res.status(500).send({"error":`parking slot with id ${id} not exists`});
     }
 }
 
